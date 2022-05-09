49b6862f0ac5dec1d50383870dc9d14103667a43
Wey Gu <weyl.gu@gmail.com>
2022-05-09 16:03:41 +0800

    Update import_dashboards.sh

--- meltano_projects/jaffle_superset/analyze/superset/import_dashboards.sh
+++ meltano_projects/jaffle_superset/analyze/superset/import_dashboards.sh
@@ -1,1 +1,1 @@
-docker compose -f analyze/superset/docker-compose.yml exec superset superset import-dashboards -p /app/assets/dashboard/dashboards.json
+docker-compose -f analyze/superset/docker-compose.yml exec superset superset import-dashboards -p /app/assets/dashboard/dashboards.json
