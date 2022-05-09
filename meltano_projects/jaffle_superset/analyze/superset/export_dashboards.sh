21e69ec869dbcdc37df05566fdf3b5a3e8440c38
Wey Gu <weyl.gu@gmail.com>
2022-05-09 16:03:57 +0800

    Update export_dashboards.sh

--- meltano_projects/jaffle_superset/analyze/superset/export_dashboards.sh
+++ meltano_projects/jaffle_superset/analyze/superset/export_dashboards.sh
@@ -1,1 +1,1 @@
-docker compose -f analyze/superset/docker-compose.yml exec superset superset export-dashboards -f /app/assets/dashboard/dashboards.json
+docker-compose -f analyze/superset/docker-compose.yml exec superset superset export-dashboards -f /app/assets/dashboard/dashboards.json
